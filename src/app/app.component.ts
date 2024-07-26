346217741577f97c1fbd87607c029e241241383c
clintkingston <ckingston1@woolworths.com.au>
2024-07-26 22:38:15 +1000

    app component cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header/header.component';
 import { FooterComponent } from './components/footer/footer/footer.component';
-import { NavigationComponent } from './components/navigation/navigation/navigation.component';
-import { AboutComponent } from './pages/about/about/about.component';
 import { SocialAccountsComponent } from './components/social-accounts/social-accounts.component';
 import { StaticContentService } from './services/static-content.service';
-import { ImageViewerComponent } from './components/image-viewer/image-viewer.component';
-import { ViewerSize, ViewerStyle } from './models/image-viewer.models';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +11,7 @@ import { ViewerSize, ViewerStyle } from './models/image-viewer.models';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    NavigationComponent,
     FooterComponent,
-    AboutComponent,
     SocialAccountsComponent,
   ],
   templateUrl: './app.component.html',
@@ -25,6 +19,7 @@ import { ViewerSize, ViewerStyle } from './models/image-viewer.models';
 })
 export class AppComponent implements OnInit {
   constructor(private staticContentService: StaticContentService) {}
+
   ngOnInit() {
     this.staticContentService.fetchStaticContent();
   }
