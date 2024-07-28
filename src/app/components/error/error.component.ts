27c67a04b4373febbf903ca343cc78bfa0c0ec87
clintkingston <ckingston1@woolworths.com.au>
2024-07-28 11:35:01 +1000

    better error component style

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,11 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './error.component.html',
   styleUrl: './error.component.css',
 })
