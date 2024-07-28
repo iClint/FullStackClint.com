f333843d4cec88388a6bf8d24055e932d6ef7a1c
clintkingston <ckingston1@woolworths.com.au>
2024-07-28 11:49:17 +1000

    accordioan logic tests

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -27,13 +27,14 @@ import { ImageViewerComponent } from '../image-viewer/image-viewer.component';
 })
 export class AccordionComponent {
   @Input({ required: true }) accordion!: ExpansionPanelModel[];
+  private hint = '... Click to read more';
 
   panelPreview(content: string, maxWords: number = 15): string {
     const words = content.split(/\s+/);
     const previewWords = words.slice(0, maxWords);
     const previewContent = previewWords.join(' ');
     return words.length > maxWords
-      ? `${previewContent} ...\nClick to read more`
-      : `${previewContent} \nClick to read more`;
+      ? `${previewContent}${this.hint}`
+      : `${previewContent}${this.hint}`;
   }
 }
